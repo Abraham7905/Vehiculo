4696e852bb561b08ccb762c03d16f7b43613677b
Aglae Duin <aglaef.duing@autonoma.co>
2024-04-01 16:02:04 -0500

    Se realizaron los métodos de acceso

--- Vehiculo/src/Autonoma/Vehiculo/Models/Vehiculo.java
+++ Vehiculo/src/Autonoma/Vehiculo/Models/Vehiculo.java
@@ -19,17 +19,54 @@ public class Vehiculo {
      */
     private String añoFabricado;
     
+    private int velocidadActual;
+    
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Método Constructor
     /**
      * 
      * @param marca
-     * @param añoFabricado 
+     * @param añoFabricado
+     * @param velocidadActual 
      */
-    public Vehiculo (String marca, String añoFabricado){
+    public Vehiculo (String marca, String añoFabricado, int velocidadActual){
     this.marca = marca;
     this.añoFabricado = añoFabricado;
+    this.velocidadActual = velocidadActual;
     }
+    
+    public Vehiculo(){
+        this.marca = "";
+        this.añoFabricado = "";
+        this.velocidadActual = 0;
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Métodos de Acesso
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getAñoFabricado() {
+        return añoFabricado;
+    }
+
+    public void setAñoFabricado(String añoFabricado) {
+        this.añoFabricado = añoFabricado;
+    }
+
+    public int getVelocidadActual() {
+        return velocidadActual;
+    }
+
+    public void setVelocidadActual(int velocidadActual) {
+        this.velocidadActual = velocidadActual;
+    }
+    
     
     
 }
